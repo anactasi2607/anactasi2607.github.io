3116b2dc88409783109afbf1c9b3aa835ca43f56
anactasi2607 <anactasi2607@yandex.ru>
2020-07-29 17:23:03 +0300

    добавила проект-таблицу

---
+++ interactive-table/precache-manifest.75be4cb74febc3307bed8d24d9470a47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378138ee8282b1ae6c13bb1f53fc5064",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e060fe4ea79402f30a",
+    "url": "/static/css/main.0b9f14a0.chunk.css"
+  },
+  {
+    "revision": "ad66eb679a40b680fe15",
+    "url": "/static/js/2.4e1b2291.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.4e1b2291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e060fe4ea79402f30a",
+    "url": "/static/js/main.b729ef0a.chunk.js"
+  },
+  {
+    "revision": "1b742e31aa59eb268be3",
+    "url": "/static/js/runtime-main.5a355c2b.js"
+  }
+]);
